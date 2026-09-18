f1fc467837f5f0ee8af0c2bc09a39aee29f78bc6
fr6382211-svg <fr6382211@gmail.com>
2026-09-18 18:49:21 +0700

    Initial commit: SchoolHub v7.2

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './styles/theme.css';
+import './styles/theme-legacy-overrides.css';
 import App from './App';
 import RuntimeErrorBoundary from './components/ui/RuntimeErrorBoundary';
 import { ToastProvider } from './components/ui/Toast';
